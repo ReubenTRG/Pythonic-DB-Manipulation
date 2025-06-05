23520e3ba0364ba96c0c55c7aaf200985b5c682b
Reuben <reubentrg45@gmail.com>
2025-06-05 09:07:10 +0400

    updated .gitignore.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pythonic_db_manipulation',         # Package name
-    version='0.1.0',                         # Version
+    name='pythonic_db_manipulation',
+    version='0.1.0',
     description='A Pythonic database manipulation library',
-    author='Your Name',                      # Your name
-    packages=find_packages(),                # Automatically find all packages
-    install_requires=[],                     # Add dependencies if needed
-    python_requires='>=3.6',                 # Minimum Python version
+    author='ReubenTRG',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.6',
 )
